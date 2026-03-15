c2e0b1131019384e007acd06d61a6f7196371747
adjeiboyejnr <adjeiboyejnr@gmail.com>
2026-03-15 15:17:13 +0300

    finish the side panel for the admin page

--- app/[locale]/dashboard/admin/layout.tsx
+++ app/[locale]/dashboard/admin/layout.tsx
@@ -3,23 +3,21 @@ import { MenuItemId } from "@/types/types";
 import { motion } from "framer-motion";
 
 import { useState, type PropsWithChildren } from "react";
-import { Sidebar } from "../components/Sidebar";
-import { MainContent } from "../components/MainContent";
-
+import { Sidebar } from "../pages/Sidebar";
+import { MainContent } from "../pages/MainContent";
 
 export default function AdminLayout({ children }: PropsWithChildren) {
-  const [selected, setSelected] = useState<MenuItemId>('dashboard');
+  const [selected, setSelected] = useState<MenuItemId>("dashboard");
   const [sidebarOpen, setsidebarOpen] = useState(true);
   return (
     <motion.div className="flex">
       <Sidebar
-       isOpen={sidebarOpen}
+        isOpen={sidebarOpen}
         onToggle={() => setsidebarOpen(!sidebarOpen)}
         selected={selected}
         onSelect={setSelected}
       />
-      <MainContent selectedItem={selected}/>
-
+      <MainContent selectedItem={selected} />
     </motion.div>
   );
 }
--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import { createBrowserClient } from '@supabase/ssr';
-import { type SupabaseClient } from '@supabase/supabase-js';
-
-let client: SupabaseClient | undefined;
-
-// Browser-side Supabase client singleton
-export function createSupabaseBrowserClient() {
-    if (client) return client;
-
-    console.log("[Supabase] Initializing singleton browser client...");
-    client = createBrowserClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    );
-
-    return client;
-=======
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
@@ -23,5 +5,4 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
->>>>>>> supabase/implementAuth
 }
