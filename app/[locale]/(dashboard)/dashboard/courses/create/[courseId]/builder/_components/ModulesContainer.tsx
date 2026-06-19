4a288385b0ab0ef045cbeaa891c8c772538fe982
adjeiboyejnr <69145017+iam-nii@users.noreply.github.com>
2026-06-19 15:10:36 +0000

    feature: add an accordion to display each module

--- app/[locale]/(dashboard)/dashboard/courses/create/[courseId]/builder/_components/ModulesContainer.tsx
+++ app/[locale]/(dashboard)/dashboard/courses/create/[courseId]/builder/_components/ModulesContainer.tsx
@@ -1,37 +1,39 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Button } from "@/components/ui/button";
 import { useCourseContentStore } from "@/store/courses/CourseContentStore"
 import { Reorder } from "framer-motion";
-import { useState } from "react";
 
 
 
 export default function ModulesContainer(){
-    const {modules, setModules} = useCourseContentStore();
-    // const [items, setItems] = useState([0, 1, 2, 3])
+    const {modules,lessons, setModules} = useCourseContentStore();
 
  
     
     return(
-        <div>
+        <div className="mt-8 md:max-w-[45vw]">
             <Reorder.Group axis="y" values={modules} onReorder={setModules}>
                 {modules.map((module) => (
                 <Reorder.Item key={module.id} value={module}>
-                    {module.title}
+                    <Accordion type="single" collapsible defaultValue="item-1">
+                        <AccordionItem value={module.id.toString()} className="border-b px-4 last:border-b-0">
+                            <AccordionTrigger>{module.title}</AccordionTrigger>
+                            <AccordionContent className="flex flex-col">
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                           
+                            <Button onClick={()=>{console.log("inner button working")}}> Add Lesson</Button>
+                            </AccordionContent>
+                        </AccordionItem>
+                        </Accordion>
                 </Reorder.Item>
                 ))}
             </Reorder.Group>
-            {/* <ul>
-            {
-            modules && modules.map((module, index)=>(
-                <li data-swapy-slot={module.id}>
-                    <div id={module.id.toString()} data-swapy-item={module.id}>
-                        <div>{module.title}</div>
-                    </div>
-                </li>
-
-                ))
-                
-            }
-            </ul> */}
+            
         </div>
     )
 }
--- store/courses/CourseContentStore.ts
+++ store/courses/CourseContentStore.ts
@@ -1,19 +1,19 @@
-import {create} from "zustand";
-import {persist, createJSONStorage} from 'zustand/middleware'
+import { create } from "zustand";
+import { persist, createJSONStorage } from 'zustand/middleware'
 
-interface Lesson{
+interface Lesson {
     title: string,
     description: string,
     video_url: string,
     video_share_url: string,
     content: string,
     files: string[],
-    order:number,
-    module_id:number
+    order: number,
+    module_id: number
 }
-interface Module{
+interface Module {
     id: number,
-    order:number,
+    order: number,
     title: string,
     lessons?: string[]
 }
@@ -21,42 +21,44 @@ interface Module{
 interface CourseContent {
     modules: Module[] | [];
     setModules: (modules: Module[]) => void;
-    lessons: Lesson[]| [];
+    lessons: Lesson[] | [];
     isLoading: boolean;
-    addModule:(title: string, id: number) => void;
-    addLesson: (lesson: Lesson)=> void;
-    updateModule: (id:number, title: string)=>void;
-    addLessonToModule: (module_id:number, lesson_id: string) => void;
+    addModule: (title: string, id: number) => void;
+    addLesson: (lesson: Lesson) => void;
+    updateModule: (id: number, title: string) => void;
+    addLessonToModule: (module_id: number, lesson_id: string) => void;
 }
 
-export const useCourseContentStore = create<CourseContent>()(persist((set)=>({
+export const useCourseContentStore = create<CourseContent>()(persist((set) => ({
     modules: [],
     lessons: [],
     isLoading: false,
     setModules: (modules) =>
-    set(() => ({
-        modules: modules.map((module, index) => ({
-        ...module,
-        order: index,
-        })),
-    })),
-    addModule: (newtitle, newId) => set((state)=>({modules:[...state.modules, {id:newId,title:newtitle,order: state.modules.length + 1, lessons:[]}]})),
-    updateModule: (module_id, module_title)=>set((state)=>({modules: state.modules.map((module)=>
-    module.id === module_id ? {...module, title:module_title} : module
-    )})),
-    addLesson: (lesson) => set((state)=>({lessons: [...state.lessons,lesson]})),
-    addLessonToModule: (module_id, lesson_id)=> set((state)=>({
-        modules: 
-        state.modules.map((module)=>{
-            if (module.id == module_id) return module;
-            if (module.lessons?.includes(lesson_id)) return module;
-            return{
+        set(() => ({
+            modules: modules.map((module, index) => ({
                 ...module,
-                lessons: [...(module.lessons || []), lesson_id]
-            }
-        })
+                order: index,
+            })),
+        })),
+    addModule: (newtitle, newId) => set((state) => ({ modules: [...state.modules, { id: newId, title: newtitle, order: state.modules.length + 1, lessons: [] }] })),
+    updateModule: (module_id, module_title) => set((state) => ({
+        modules: state.modules.map((module) =>
+            module.id === module_id ? { ...module, title: module_title } : module
+        )
+    })),
+    addLesson: (lesson) => set((state) => ({ lessons: [...state.lessons, lesson] })),
+    addLessonToModule: (module_id, lesson_id) => set((state) => ({
+        modules:
+            state.modules.map((module) => {
+                if (module.id == module_id) return module;
+                if (module.lessons?.includes(lesson_id)) return module;
+                return {
+                    ...module,
+                    lessons: [...(module.lessons || []), lesson_id]
+                }
+            })
     }))
 
-}),{
-    name:'course-content-storage',
+}), {
+    name: 'course-content-storage',
 }))
